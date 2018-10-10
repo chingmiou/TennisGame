16733441b2281e0e4d81940b1767de7db393af0c
Janet Wang <ad.janetwang@gmail.com>
2018-10-11 01:16:51 +0800

    pass

---
+++ UnitTestProject1/UnitTestProject1/TennisGame.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitTestProject1
+{
+    public class TennisGame
+    {
+        public string Score()
+        {
+            return "Love All";
+        }
+    }
+}
--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1
 {
     [TestClass]
-    public class UnitTest1
+    public class TennisGameTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Love_All()
         {
+            var tennisGame = new TennisGame();
+            var score = tennisGame.Score();
+            Assert.AreEqual("Love All", score);
         }
     }
 }
