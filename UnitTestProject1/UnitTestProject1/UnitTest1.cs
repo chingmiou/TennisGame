475e91cdb5088b7b5b0e93800686c9264c79c66b
Janet Wang <ad.janetwang@gmail.com>
2018-10-12 00:49:20 +0800

    pass

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -13,6 +13,13 @@ namespace UnitTestProject1
             ScoreShouldBe("Love All");
         }
 
+        [TestMethod]
+        public void Fifteen_Love()
+        {
+            _tennisGame.FirstPlayerScore();
+            ScoreShouldBe("Fifteen Love");
+        }
+
         private void ScoreShouldBe(string expected)
         {
             Assert.AreEqual(expected, _tennisGame.Score());
