273d41321420a5ee1af9b3a50e09f6944fc1b97a
Janet Wang <ad.janetwang@gmail.com>
2018-10-30 21:37:58 +0800

    20181028

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -1,14 +1,125 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1
 {
     [TestClass]
-    public class UnitTest1
+    public class TennisTests
     {
+        private Tennis _tennis = new Tennis("Joey", "Tom");
+
         [TestMethod]
-        public void TestMethod1()
+        public void Love_All()
         {
+            ScoreShouldBe("Love All");
+        }
+
+        [TestMethod]
+        public void Fifteen_Love()
+        {
+            GivenFirstPlayerScoreTimes(1);
+            ScoreShouldBe("Fifteen Love");
+        }
+
+        [TestMethod]
+        public void Thirty_Love()
+        {
+            GivenFirstPlayerScoreTimes(2);
+            ScoreShouldBe("Thirty Love");
+        }
+
+        [TestMethod]
+        public void Forty_Love()
+        {
+            GivenFirstPlayerScoreTimes(3);
+            ScoreShouldBe("Forty Love");
+        }
+
+        [TestMethod]
+        public void Love_Fifteen()
+        {
+            GivenSecondPlayerScoreTimes(1);
+            ScoreShouldBe("Love Fifteen");
+        }
+
+        [TestMethod]
+        public void Love_Thirty()
+        {
+            GivenSecondPlayerScoreTimes(2);
+            ScoreShouldBe("Love Thirty");
+        }
+
+        [TestMethod]
+        public void Fifteen_All()
+        {
+            GivenFirstPlayerScoreTimes(1);
+            GivenSecondPlayerScoreTimes(1);
+            ScoreShouldBe("Fifteen All");
+        }
+
+        [TestMethod]
+        public void Thirty_All()
+        {
+            GivenFirstPlayerScoreTimes(2);
+            GivenSecondPlayerScoreTimes(2);
+            ScoreShouldBe("Thirty All");
+        }
+
+        [TestMethod]
+        public void Deuce()
+        {
+            GivenDeuce();
+            ScoreShouldBe("Deuce");
+        }
+
+        private void GivenDeuce()
+        {
+            GivenFirstPlayerScoreTimes(3);
+            GivenSecondPlayerScoreTimes(3);
+        }
+
+        [TestMethod]
+        public void FirstPlayer_Adv()
+        {
+            GivenDeuce();
+            GivenFirstPlayerScoreTimes(1);
+            ScoreShouldBe("Joey Adv");
+        }
+
+        [TestMethod]
+        public void SecondPlayer_Adv()
+        {
+            GivenDeuce();
+            GivenSecondPlayerScoreTimes(1);
+            ScoreShouldBe("Tom Adv");
+        }
+
+        [TestMethod]
+        public void SecondPlayer_Win()
+        {
+            GivenDeuce();
+            GivenSecondPlayerScoreTimes(2);
+            ScoreShouldBe("Tom Win");
+        }
+
+        private void GivenSecondPlayerScoreTimes(int times)
+        {
+            for (var i = 0; i < times; i++)
+            {
+                _tennis.SecondPlayerScore();
+            }
+        }
+
+        private void GivenFirstPlayerScoreTimes(int times)
+        {
+            for (var i = 0; i < times; i++)
+            {
+                _tennis.FirstPlayerScore();
+            }
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, _tennis.Score());
         }
     }
 }
