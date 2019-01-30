417e96e4b378faa5d4cdd2ed31c6ee67dc1a59d3
ching.miou <ching.miou@gmail.com>
2019-01-30 22:12:14 +0800

    add PostfixTemplate

---
+++ UnitTestProject1/UnitTestProject1/PostfixTemplate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace UnitTestProject1
+{
+    public static class PostfixTemplate
+    {
+        [JetBrains.Annotations.SourceTemplateAttribute]
+        [Macro(Target = "expected", Editable = 1)]
+        public static void ae<T>(this T source)
+        {
+            /*$ Assert.AreEqual( $expected$ , source);*/
+        }
+
+        [JetBrains.Annotations.SourceTemplateAttribute]
+        public static void abs<T>(this T source)
+        {
+            /*$ Math.Abs(source) */
+        }
+    }
+}
